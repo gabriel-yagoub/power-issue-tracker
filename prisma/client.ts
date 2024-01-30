9ac59b8332bc2a91866cd472ab503512bb2712c9
Gabriel Yagoub <gabrielyagoub@gmail.com>
2024-01-30 19:34:39 +0100

    API for creating issues version 1.0

---
+++ prisma/client.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+
+// The purpose of this code is to ensure that we only have on instance of Prisma running
