8f84b4b0ed124db63ed326f72b64da6465d91d91
Gabriel Yagoub <gabrielyagoub@gmail.com>
2024-01-31 13:36:34 +0100

    Handle Form Submission to DB OK

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,35 @@
 'use client'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-
-
-
+import { useForm, Controller } from "react-hook-form";
 import { Button, TextField } from '@radix-ui/themes'
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const {register, control, handleSubmit} = useForm<IssueForm>();
+
   return (
-    <div className='max-w-xl space-y-3'>
+    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data) => {
+      await axios.post('/api/issues', data)
+      router.push('/issues');
+    })}>
         <TextField.Root>
-           <TextField.Input placeholder='Titel' /> 
+           <TextField.Input placeholder='Titel' {...register('title')} />
         </TextField.Root>
-        <SimpleMDE placeholder='Beskriv ditt arande' />
+        <Controller
+          name = "description"
+          control = {control}
+          render={({ field }) => <SimpleMDE placeholder='Beskriv ditt arende' {...field}/>}
+        />
         <Button>Skapa Nytt Arande</Button>
-    </div>
+    </form>
   )
 }
 
