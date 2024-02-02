b347864a21dda1832b02226846c1bff4a74ca7fd
Gabriel Yagoub <gabrielyagoub@gmail.com>
2024-02-02 19:20:04 +0100

    Implemented Delete button

---
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@radix-ui/themes";
+
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  return <Button color="red">Radera ärende</Button>;
+};
+
+export default DeleteIssueButton;
