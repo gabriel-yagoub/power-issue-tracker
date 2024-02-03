8cf8f1be9da59fc55636f6fdb711ce797cf03c44
Gabriel Yagoub <gabrielyagoub@gmail.com>
2024-02-03 16:42:01 +0100

    Implemented protected routes

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,18 +1,26 @@
 import { issueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
+
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
+
   if (!issue)
     return NextResponse.json({ error: "Issue not found" }, { status: 400 });
   const updatedIssue = await prisma.issue.update({
@@ -29,6 +37,9 @@ export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) return NextResponse.json({}, { status: 401 });
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
