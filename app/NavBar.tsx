aa6d4cc5c54f6cb97e0636df0859fa172c0fdba2
Gabriel Yagoub <gabrielyagoub@gmail.com>
2024-02-03 16:18:29 +0100

    Implemented loading skeleteon for loginbtn

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -16,6 +16,7 @@ import {
   Text,
   Button,
 } from "@radix-ui/themes";
+import { Skeleton } from "@/app/components";
 
 const NavBar = () => {
   return (
@@ -65,7 +66,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession();
 
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width="5rem" height="2rem" />;
 
   if (status === "unauthenticated")
     return (
