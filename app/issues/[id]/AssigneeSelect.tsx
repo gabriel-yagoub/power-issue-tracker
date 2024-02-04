3576748a09f32eb749a13e13545c7549ca9752a0
Gabriel Yagoub <gabrielyagoub@gmail.com>
2024-02-04 17:16:09 +0100

    Refactor code, pain in the ass

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,51 +1,43 @@
 "use client";
 
+import { Skeleton } from "@/app/components";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Skeleton } from "@/app/components";
 import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000,
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton />;
 
   if (error) return null;
 
+  const assignIssue = (userId: string) => {
+    axios
+      .patch("/api/issues/" + issue.id, {
+        assignedToUserId: userId || null,
+      })
+      .catch(() => {
+        toast.error("Gick inte att spara ändringarna");
+      });
+    {
+      if (userId) {
+        users?.map((user) =>
+          toast.success("Ärendet har tilldelats " + user.name)
+        );
+      } else toast.error("Ärendet har inte tilldelats");
+    }
+  };
+
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || ""}
-        onValueChange={(userId) => {
-          axios
-            .patch("/api/issues/" + issue.id, {
-              assignedToUserId: userId || null,
-            })
-            .catch(() => {
-              toast.error("Gick inte att spara ändringarna");
-            });
-          {
-            if (userId) {
-              users?.map((user) =>
-                toast.success("Ärendet har tilldelats " + user.name)
-              );
-            } else toast.error("Ärendet har inte tilldelats");
-          }
-        }}
+        onValueChange={assignIssue}
       >
-        <Select.Trigger placeholder="Tilldela" />
+        <Select.Trigger placeholder="Tilldela ärende" />
         <Select.Content>
           <Select.Group>
             <Select.Label>Förslag</Select.Label>
@@ -62,5 +54,13 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 
 export default AssigneeSelect;
