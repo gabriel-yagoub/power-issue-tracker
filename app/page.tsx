ccb8fbcc66e2a282d4ff888a12cd21e624bf4752
Gabriel Yagoub <gabrielyagoub@gmail.com>
2024-02-07 13:56:31 +0100

    Bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,50 @@ import { Flex, Grid } from "@radix-ui/themes";
 import LatestIssues from "./LatestIssues";
 import { Metadata } from "next";
 
+export default async function Home({ issues }) {
+  const { open, inProgress, closed } = issues;
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
+}
+
+export const getServerSideProps = async () => {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+
+  return {
+    props: {
+      issues: { open, inProgress, closed },
+    },
+    // Add cache-control headers to prevent caching
+    headers: {
+      "Cache-Control": "no-store, must-revalidate",
+    },
+  };
+};
+
+export const metadata: Metadata = {
+  title: "Power Issue Tracker - Dashboard",
+  description: "Overview of the current issues and status",
+};
+
+/*
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
+import IssueChart from "./IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
+import LatestIssues from "./LatestIssues";
+import { Metadata } from "next";
+
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
   const inProgress = await prisma.issue.count({
@@ -26,3 +70,4 @@ export const metadata: Metadata = {
   title: "Power Issue Tracker - Dashboard",
   description: "Overview of the current issues and status",
 };
+*/
