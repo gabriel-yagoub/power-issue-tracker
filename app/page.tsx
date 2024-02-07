9765eb69ff4c84cd3f3ff84bf3eb88cb1f8dfcdc
Gabriel Yagoub <gabrielyagoub@gmail.com>
2024-02-07 14:08:55 +0100

    Bugfix homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,62 +5,6 @@ import { Flex, Grid } from "@radix-ui/themes";
 import LatestIssues from "./LatestIssues";
 import { Metadata } from "next";
 
-interface Issues {
-  open: number;
-  inProgress: number;
-  closed: number;
-}
-
-interface HomeProps {
-  issues?: Issues; // Make issues optional
-}
-
-export default function Home({ issues }: HomeProps) {
-  // Check if issues is undefined, use default values if so
-  const { open = 0, inProgress = 0, closed = 0 } = issues || {};
-
-  return (
-    <Grid columns={{ initial: "1", md: "2" }} gap="5">
-      <Flex direction="column" gap="5">
-        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
-        <IssueChart open={open} inProgress={inProgress} closed={closed} />
-      </Flex>
-      <LatestIssues />
-    </Grid>
-  );
-}
-
-export const getServerSideProps = async () => {
-  const open = await prisma.issue.count({ where: { status: "OPEN" } });
-  const inProgress = await prisma.issue.count({
-    where: { status: "IN_PROGRESS" },
-  });
-  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
-
-  return {
-    props: {
-      issues: { open, inProgress, closed },
-    },
-    // Add cache-control headers to prevent caching
-    headers: {
-      "Cache-Control": "no-store, must-revalidate",
-    },
-  };
-};
-
-export const metadata: Metadata = {
-  title: "Power Issue Tracker - Dashboard",
-  description: "Overview of the current issues and status",
-};
-
-/*
-import prisma from "@/prisma/client";
-import IssueSummary from "./IssueSummary";
-import IssueChart from "./IssueChart";
-import { Flex, Grid } from "@radix-ui/themes";
-import LatestIssues from "./LatestIssues";
-import { Metadata } from "next";
-
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
   const inProgress = await prisma.issue.count({
@@ -82,4 +26,3 @@ export const metadata: Metadata = {
   title: "Power Issue Tracker - Dashboard",
   description: "Overview of the current issues and status",
 };
-*/
